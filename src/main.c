73c96adfdc9d3564366e7bd0be6efe662415ae5f
Aylin Yoztyurk <ozturkaylin18@gmail.com>
2025-10-20 18:15:24 +0200

    Lab01 with Timer as a counter

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <xc.h>
 #include <p33Fxxxx.h>
+
 //do not change the order of the following 2 definitions
 #define FCY 12800000UL 
 #include <libpic30.h>
@@ -8,63 +9,81 @@
 #include "led.h"
 
 /* Configuration of the Chip */
-// Initial Oscillator Source Selection = Primary (XT, HS, EC) Oscillator with PLL
-#pragma config FNOSC = PRIPLL
-// Primary Oscillator Mode Select = XT Crystal Oscillator mode
-#pragma config POSCMD = XT
-// Watchdog Timer Enable = Watchdog Timer enabled/disabled by user software
-// (LPRC can be disabled by clearing the SWDTEN bit in the RCON register)
-#pragma config FWDTEN = OFF
+#pragma config FNOSC = PRIPLL   // Initial Oscillator Source Selection = Primary Oscillator with PLL
+#pragma config POSCMD = XT      // Primary Oscillator Mode = XT
+#pragma config FWDTEN = OFF     // Watchdog Timer Enable = OFF
 
+void timer1_init(void);
+void __attribute__((__interrupt__)) _T1Interrupt(void);
+void run_counter_on_led(void);
 
-volatile int counter=0;
-int main(){
-    //Init LCD and LEDs
+volatile int counter = 0;
+
+int main() {
+    // Init LCD and LEDs
     lcd_initialize();
     led_init();
-	
-    // Clear the Screen and reset the cursor
+
     lcd_clear();
     lcd_locate(0, 0);
     lcd_printf("Group members#");
-    
+
     lcd_locate(0, 1);
     lcd_printf("AYLIN");
-    
+
     lcd_locate(0, 2);
     lcd_printf("LAVINDA");
-    
+
     lcd_locate(0, 3);
-    lcd_printf("AHMET ");
-    
+    lcd_printf("AHMET");
+
     __delay_ms(2000);
-    
+
     CLEARLED(LED1_TRIS);
     CLEARLED(LED2_TRIS);
     CLEARLED(LED3_TRIS);
     CLEARLED(LED4_TRIS);
     CLEARLED(LED5_TRIS);
-    while(1){
-        lcd_locate(0, 6);
-        
-        lcd_printf("Counter value=%3u",counter);
-        
-        
-        LATAbits.LATA4=(counter>>0)&1;
-        LATAbits.LATA5=(counter>>1)&1;
-        LATAbits.LATA9=(counter>>2)&1;
-        LATAbits.LATA10=(counter>>3)&1;
-        LATAbits.LATA0=(counter>>4)&1;
-        
-        counter++ ; 
-        if(counter>32){
-            counter=0x00;
-            
-        }
-        
-        __delay_ms(300);
+
+    timer1_init();
+
+    while (1) {
+        run_counter_on_led();
     }
+
     return 0;
 }
 
-    
+void run_counter_on_led(void){
+    lcd_locate(0, 6);
+        lcd_printf("Counter value=%3u", counter);
+
+        LATAbits.LATA4  = (counter >> 0) & 1;
+        LATAbits.LATA5  = (counter >> 1) & 1;
+        LATAbits.LATA9  = (counter >> 2) & 1;
+        LATAbits.LATA10 = (counter >> 3) & 1;
+        LATAbits.LATA0  = (counter >> 4) & 1;
+
+        
+   
+}
+void timer1_init(void) {
+    CLEARBIT(T1CONbits.TON);
+    CLEARBIT(T1CONbits.TCS);
+    CLEARBIT(T1CONbits.TGATE);
+    TMR1 = 0x00;
+    T1CONbits.TCKPS = 0b11;    // 1:256 prescaler
+    PR1 = 100000;               // Period
+    IPC0bits.T1IP = 0x01;      // Priority
+    IFS0bits.T1IF = 0;         // Clear flag
+    IEC0bits.T1IE = 1;         // Enable interrupt
+    SETBIT(T1CONbits.TON);     // Start Timer
+}
+
+void __attribute__((__interrupt__)) _T1Interrupt(void) {
+    counter++; 
+    if (counter > 32) {
+        counter = 0;
+    }
+    IFS0bits.T1IF = 0; // Clear interrupt flag
+}
