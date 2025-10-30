66fbe92cf6c58c8745296f49bad2b5a85cacdbce
Lavinda Gouvandi Susanto <lavindags@gmail.com>
2025-10-30 14:26:55 +0100

    1st attempt

--- src/lab02.c
+++ src/lab02.c
@@ -22,27 +22,72 @@ void initialize_timer()
     // afterwards writes the value val to that register. (OSCCONL represents the
     // lower 8 bits of the register OSCCON)
     __builtin_write_OSCCONL(OSCCONL | 2);
-    // Disable the Timers
+
+    // Disable the Timers 
+    CLEARBIT(T2CONbits.TON);
+    T1CONbits.TON = 0; 
+    T3CONbits.TON = 0; 
+    
     
     // Set Prescaler
+    //1
+    T3CONbits.TCKPS = 0b00;
+
+    //256
+    T2CONbits.TCKPS = 0b11;
+
+    //256
+    T1CONbits.TCKPS = 0b11;
 
     // Set Clock Source
+    CLEARBIT(T2CONbits.TCS); //internal 12 mhz
+    T1CONbits.TCS = 1; //external 32k
+    CLEARBIT(T3CONbits.TCS); //internal 12 mhz
     
     // Set Gated Timer Mode -> don't use gating
+    CLEARBIT(T2CONbits.TGATE);
+    CLEARBIT(T1CONbits.TGATE);
+    CLEARBIT(T3CONbits.TGATE);
+    
 
     // T1: Set External Clock Input Synchronization -> no sync
+    T1CONbits.TSYNC = 0;
 
     // Load Timer Periods
+    PR2 = 936; // 2ms
+    PR1 = 127; //1s
+    PR3 = 65535; // highest period possible, 5ms
     
     // Reset Timer Values
+    TMR1 = 0x00;
+    TMR2 = 0x00;
+    TMR3 = 0x00;
 
     // Set Interrupt Priority
+    IPC0bits.T2IP = 0x01; 
+    IPC0bits.T1IP = 0x01;
+    IPC0bits.T3IP = 0x01;
 
     // Clear Interrupt Flags
+    CLEARBIT(IFS0bits.T2IF);
+    IFS0bits.T1IF = 0;
+    CLEARBIT(IFS0bits.T3IF);
 
     // Enable Interrupts
+    SETBIT(IEC0bits.T2IE);
+    IEC0bits.T1IE = 1;
+    SETBIT(IEC0bits.T3IE);
 
     // Enable the Timers
+    SETBIT(T2CONbits.TON);
+    T1CONbits.TON = 1;
+    SETBIT(T3CONbits.TON);
+
+    //machine is Fcy = 12,8 MHz
+    //Timer1 with 1:256 prescaler
+    //T =  (PR1 + 1) x tick period
+    // tick period = prescaler / Fcy = 256 / 12,800,000 = 20 µs
+    
 
 }
 
@@ -51,7 +96,7 @@ void timer_loop()
     // print assignment information
     lcd_printf("Lab02: Int & Timer");
     lcd_locate(0, 1);
-    lcd_printf("Group: GroupName");
+    lcd_printf("Group: Aylin /r Ahmet /r Lavinda /r");
     
     while(TRUE)
     {
--- src/main.c
+++ src/main.c
@@ -19,6 +19,11 @@
 // (LPRC can be disabled by clearing the SWDTEN bit in the RCON register)
 #pragma config FWDTEN = OFF
 
+
+volatile uint16_t counterInter1=0;
+volatile uint16_t counterInter2=0;
+volatile uint16_t counterInter3=0;
+
 int main(){
     //Init LCD and LEDs
     lcd_initialize();
@@ -33,9 +38,93 @@ int main(){
     
     // Start Lab02 Main Program
     timer_loop();
+    uint16_t counter1;
+    uint16_t counter2;
+    uint16_t counter3;
+
+    //LED ON
+    CLEARLED(LED1_TRIS); // Set Pin to Output
+    CLEARLED(LED2_TRIS); // Set Pin to Output
+    CLEARLED(LED3_TRIS); // Set Pin to Output
+    CLEARLED(LED4_TRIS); // Set Pin to Output
+    CLEARLED(LED5_TRIS); // Set Pin to Output
+    //LED
     
     // Stop
     while(1)
-        ;
+
+    if (counterInter1 ==1){
+            counterInter1=0;
+            counter1++;
+            if (counter1%2==0){
+                SETLED(LED1_PORT);} 
+            else{
+            CLEARLED(LED1_PORT);}
+
+            
+
+
+
+    }
+    
+    if (counterInter2 ==1){
+        counterInter2=0;
+        counter2++;
+        if (counter2%2==0){
+            SETLED(LED2_PORT);} 
+        else{
+            CLEARLED(LED2_PORT);}
+
+    }
+
+    if (counterInter2 ==1){
+
+        counterInter2=0;
+        counter2++;
+
+    if (counter3%2==0){
+
+        if(counter3%2000==0){
+
+            SETLED(LED3_PORT);} 
+        else{
+            CLEARLED(LED3_PORT);}}
+
+    }
+
+    lcd_locate(0,5);
+    lcd_printf("%02d : %02d .  %03d",TMR1,TMR2,TMR3);
+}
+
+
+void __attribute__ (( interrupt )) _T1Interrupt(void){
+
+    counterInter1=1;
+
+    IFS0bits.T1IF = 0;
+
+
+}
+
+void __attribute__ (( interrupt )) _T2Interrupt(void){
+
+    counterInter2=1;
+    
+
+    IFS0bits.T2IF = 0;
+
+
+}
+
+void __attribute__ (( interrupt )) _T3Interrupt(void){
+
+    counterInter3=1;
+
+
+    
+
+    IFS0bits.T3IF = 0;
+
+
 }
 
